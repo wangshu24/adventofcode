0e2c1f3c0f2c06d02f6b9e6c27434d2e055f6b9a
wangshu24 <trandinhphuc1997@gmail.com>
2025-12-03 20:33:13 -0500

    [SOLVED] part 1 of day 2, counting all invalid id and take sums all fo said ids

--- 2025/bash/day2/part1.sh
+++ 2025/bash/day2/part1.sh
@@ -1,11 +1,21 @@
+# read -a input < example.txt
 read -a input < input.txt
 input="${input//,/-}"
 IFS='-' read -r -a arr <<< "$input"
 
 total=0
 for (( i=0; i<${#arr[@]}; i+=2)); do
-    echo ${arr[@]:i:2}
-    
+    start=${arr[i]}
+    end=${arr[i+1]}
+    for (( j=start; j<=end; j++ )); do
+        len=${#j}
+        l=${j:0:len/2}
+        r=${j:len/2:len/2}
+        if [[ ${j:0:len/2} == ${j:len/2:len/2} && $(( len % 2 )) -eq 0 ]]; then
+            echo "Invalid id: ${j}"
+            total=$(( total + j ))
+        fi
+    done
 done
 
 echo "Sum: ${total}"
