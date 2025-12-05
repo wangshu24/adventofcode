d60559ec07bc79c1ff5453d4c76732176d8aa564
wangshu24 <trandinhphuc1997@gmail.com>
2025-12-04 22:10:49 -0500

    wip solution for part 2 of day 3

---
+++ 2025/bash/day3/part2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mapfile -t input < example.txt
+
+total=0
+for line in "${input[@]}"; do
+    line=${line%$'\r'}
+    prev=-1
+    tmp=0
+    for (( pos=12; pos > 0; pos-- )); do
+        most=$(( prev + 1 ))
+        for (( i=$(( prev + 1 )); i <= $(( line - pos )); i++ )); do
+            if [[ ${line:most:1} -lt ${line:i:1} ]]; then
+                most=$i
+            fi
+        done
+        tmp=$(( tmp * 10 + ${line:most:1} ))
+        echo "new tmp: $tmp"
+        prev=$most
+    done
+    total=$(( total + $tmp ))
+done
+
+echo "total: $total"
